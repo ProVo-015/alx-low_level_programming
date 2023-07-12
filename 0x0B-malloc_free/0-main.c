4c932258c8b0f733638132f38716ac20fd126402
ProVo-015 <nsmalatja@gmail.com>
2023-07-12 16:03:24 -0700

    update

--- 0x0B-malloc_free/0-main.c
+++ 0x0B-malloc_free/0-main.c
@@ -10,24 +10,22 @@
 * Return: Nothing.
 */
 void simple_print_buffer(char *buffer, unsigned int size)
+{unsigned int i;
+i = 0;
+while (i < size)
 {
-   unsigned int i;
-
-   i = 0;
-   while (i < size)
-   {
-       if (i % 10)
-       {
-           printf(" ");
-       }
-       if (!(i % 10) && i)
-       {
-           printf("\n");
-       }
-       printf("0x%02x", buffer[i]);
-       i++;
-   }
-   printf("\n");
+if (i % 10)
+{
+printf(" ");
+}
+if (!(i % 10) && i)
+{
+printf("\n");
+}
+printf("0x%02x", buffer[i]);
+i++;
+}
+printf("\n");
 }
 
 /**
@@ -36,16 +34,14 @@ void simple_print_buffer(char *buffer, unsigned int size)
 * Return: Always 0.
 */
 int main(void)
-{
-   char *buffer;
+{char *buffer;
+buffer = create_array(98, 'H');
 
-   buffer = create_array(98, 'H');
-   if  (buffer == NULL)
-   {
-       printf("failed to allocate memory\n");
-       return (1);
-   }
-   simple_print_buffer(buffer, 98);
-   free(buffer);
-   return (0);
+if  (buffer == NULL)
+{
+printf("failed to allocate memory\n");
+return (1);}
+simple_print_buffer(buffer, 98);
+free(buffer);
+return (0);
 }
