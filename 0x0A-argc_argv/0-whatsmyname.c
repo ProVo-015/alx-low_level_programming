b742e477e2cc3d04922b6661c30eabfc54d16de9
ProVo-015 <nsmalatja@gmail.com>
2023-07-12 15:11:42 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,7 +2,7 @@
 
 
 /**
-* main - prints the number of arguments passed into it.
+* main - prints its name, followed by a new line.
 * @argc: argument count
 * @argv: arguments
 *
@@ -10,9 +10,9 @@
 */
 int main(int argc, char **argv)
 {
-(void)argv;
+(void)argc;
 
 
-printf("%d\n", argc - 1);
+printf("%s\n", argv[0]);
 return (0);
 }
