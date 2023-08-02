f981d9c13b161ef2078c43b6e85cae61bad5d25f
ProVo-015 <nsmalatja@gmail.com>
2023-08-02 11:04:44 -0700

    ead

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,13 +3,13 @@
 /**
  * insert_nodeint_at_index - inserts a new node in a linked list,
  * at a given position
- * @eye: pointer to the first node in the list
+ * @ear: pointer to the first node in the list
  * @idx: index where the new node is added
  * @n: data to insert in the new node
  *
  * Return: pointer to the new node, or NULL
  */
-listint_t *insert_nodeint_at_index(listint_t **eye, unsigned int idx, int n)
+listint_t *insert_nodeint_at_index(listint_t **ear, unsigned int idx, int n)
 {
 	unsigned int i;
 	listint_t *new;
